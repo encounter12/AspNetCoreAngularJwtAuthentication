1cd3755f43be46c228cbd73a6d2fa18001fa2997
Ivaylo Botusharov <ivaylo.botusharov@gmail.com>
2018-11-26 23:40:02 +0200

    added ReportController and longer secret key

---
+++ server/AspNetCoreJwtAuthentication.Api/Controllers/ReportController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreJwtAuthentication.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
